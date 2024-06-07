087adb42b43c27f23ca04ca6f831695cde7ff3b0
Adrian Fota <adrian.mihael.fota@gmail.com>
2024-06-07 22:07:55 +0300

    The tree is now saved to a file and then loaded from the file on a new run

---
+++ README.md
@@ -0,0 +1,5 @@
+Guessinator - a small C algorithm that tries to guess what person you are thinking about by asking you questions.
+
+Every time you add a person it is saved to a binary tree (and then into a file). The more people you add, the more complex the tree becomes.
+
+After exiting, the tree will be saved to save.dat
--- guessinator.c
+++ guessinator.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct node {
+typedef struct Node {
     char *question;
-    struct node *no;
-    struct node *yes;
-} node;
+    struct Node *no;
+    struct Node *yes;
+} Node;
 
 int yes_or_no(char *question) {
     char answer[3];
@@ -15,44 +15,15 @@ int yes_or_no(char *question) {
     return answer[0] == 'y';
 }
 
-node* create_new_node(char *question) {
-    node *new_node = malloc(sizeof(node));
+Node* create_new_node(char *question) {
+    Node *new_node = (struct Node*)malloc(sizeof(Node));
     new_node->question = strdup(question);
     new_node->no = NULL;
     new_node->yes = NULL;
     return new_node;
 }
 
-/*
-void save_to_file(node *start, FILE *savefile) {
-    if (start == NULL) {
-        fprintf(savefile, "# | ");
-        return;
-    }
-    fprintf(savefile, "%s | ", start->question);
-    save_to_file(start->no, savefile);
-    save_to_file(start->yes, savefile);
-}
-
-node* load_from_file(FILE *loadfile) {
-    char buffer[256];
-    if (fscanf(loadfile, "%255[^|] | ", buffer) != 1) {
-        return NULL;
-    }
-
-    if (strcmp(buffer, "#") == 0) {
-        return NULL;
-    }
-
-    node *new_node = create_new_node(buffer);
-    new_node->no = load_from_file(loadfile);
-    new_node->yes = load_from_file(loadfile);
-
-    return new_node;
-}
-*/
-
-void release(node *n) {
+void release(Node *n) {
     if (n) {
         if (n->no) release(n->no);
         if (n->yes) release(n->yes);
@@ -61,39 +32,48 @@ void release(node *n) {
     }
 }
 
+void save_to_file(struct Node* n, FILE* savefile) {
+    if(n == NULL) return;
+    int hasChildren = (n->yes && n->no);
+    fprintf(savefile, "%s | %i ", n->question, hasChildren);
+    save_to_file(n->yes, savefile);
+    save_to_file(n->no, savefile);
+}
+
+int read_next_node(FILE* file, char* question, int* hasChildren) {
+    return fscanf(file, " %[^|] | %i ", question, hasChildren);
+}
+
+struct Node* load_from_file(FILE* savefile) {
+    char question[80];
+    int hasChildren;
+    if(read_next_node(savefile, question, &hasChildren) != 2) return NULL;
+    struct Node* n = create_new_node(question);
+    if(hasChildren) {
+        n->yes = load_from_file(savefile);
+        n->no = load_from_file(savefile);
+    }
+    return n;
+}
+
 int main() {
     char question[80];
     char person[20];
-    //node *starting_node = NULL;
-    //FILE *loadfile = fopen("savefile.dat", "r");
-    node *starting_node = create_new_node("Is the person a man");
-    starting_node->no = create_new_node("Joanne Doe");
-    starting_node->yes = create_new_node("John Doe");
 
-    /*
-    if (loadfile) {
-        fseek(loadfile, 0, SEEK_END);
-        int filesize = ftell(loadfile);
-        fseek(loadfile, 0, SEEK_SET);
-        if (filesize > 0) {
-            starting_node = load_from_file(loadfile);
-        } else {
-            starting_node = create_new_node("Is the person a man");
-            starting_node->no = create_new_node("Joanne Doe");
-            starting_node->yes = create_new_node("John Doe");
-        }
-        fclose(loadfile);
-    } else {
-        starting_node = create_new_node("Is the person a man");
-        starting_node->no = create_new_node("Joanne Doe");
-        starting_node->yes = create_new_node("John Doe");
+    FILE* loadfile = fopen("save.dat", "r");
+    Node* loaded_root = load_from_file(loadfile);
+    fclose(loadfile);
+
+    if(loaded_root == NULL) {
+        loaded_root = create_new_node("Is a man");
+        loaded_root->yes = create_new_node("John Doe");
+        loaded_root->no = create_new_node("Joanne Doe");
     }
-    */
 
-    node *current_node;
+    Node *current_node;
 
     do {
-        current_node = starting_node;
+        current_node = loaded_root;
         while (1) {
             if (yes_or_no(current_node->question)) {
                 if (current_node->yes) current_node = current_node->yes;
@@ -106,9 +86,9 @@ int main() {
                 printf("Who is the person? ");
                 fgets(person, 20, stdin);
                 strtok(person, "\n");
-                node *yes_node = create_new_node(person);
+                Node *yes_node = create_new_node(person);
                 current_node->yes = yes_node;
-                node *no_node = create_new_node(current_node->question);
+                Node *no_node = create_new_node(current_node->question);
                 current_node->no = no_node;
                 printf("What would be TRUE for %s but FALSE for %s?\n", person, current_node->question);
                 fgets(question, 80, stdin);
@@ -120,16 +100,9 @@ int main() {
         }
     } while (yes_or_no("Run again"));
 
-    /*
-    FILE *savefile = fopen("savefile.dat", "w");
-    if (!savefile) {
-        perror("Error opening the file");
-        return 1;
-    }
-    save_to_file(starting_node, savefile);
+    FILE* savefile = fopen("save.dat", "w");
+    save_to_file(loaded_root, savefile);
     fclose(savefile);
-    */
-   
-    release(starting_node);
+    release(loaded_root);
     return 0;
 }
